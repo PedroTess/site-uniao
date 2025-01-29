35615805295522a2b0a076b2ef14aafe43453eb2
PedroTess <pedrotessniari@gmail.com>
2025-01-29 17:56:31 -0300

    Add files via upload

---
+++ js/reveal.js
@@ -0,0 +1,68 @@
+window.revelar = ScrollReveal ({reset:true})
+
+// Serviços
+revelar.reveal ('.effect-txt-sv',{
+    duration: 1000,
+    distance: '20px'
+})
+
+revelar.reveal ('.effect-services-img1',{
+    duration: 1000,
+    distance: '20px',
+})
+
+revelar.reveal ('.effect-services-img2',{
+    duration: 1000,
+    distance: '20px',
+    delay: 300
+})
+
+revelar.reveal ('.effect-services-img3',{
+    duration: 1000,
+    distance: '20px',
+    delay: 600
+})
+
+// Avaliações 
+
+revelar.reveal ('.effect-txt-ava',{
+    duration: 1000,
+    distance: '20px'
+})
+
+revelar.reveal ('.effect-feedback-img1',{
+    duration: 1000,
+    distance: '20px',
+})
+
+revelar.reveal ('.effect-feedback-img2',{
+    duration: 1000,
+    distance: '20px',
+    delay: 300
+})
+
+revelar.reveal ('.effect-feedback-img3',{
+    duration: 1000,
+    distance: '20px',
+    delay: 600
+})
+
+// Quem somos
+
+revelar.reveal ('.effect-txt-aboutus',{
+    duration: 1500,
+    distance: '30px',
+    origin: 'left'
+})
+
+revelar.reveal ('.effect-txt-insta',{
+    duration: 1500,
+    distance: '30px',
+    origin: 'left'
+})
+
+revelar.reveal ('.effect-img-aboutus',{
+    duration: 1500,
+    distance: '30px',
+    origin: 'right'
+})
--- js/sript.js
+++ js/sript.js
@@ -20,4 +20,28 @@ document.querySelectorAll('.mobile-menu .nav-item a').forEach(item => {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+    const header = document.querySelector("header"); // Pegando o header para calcular o tamanho
+  
+    navLinks.forEach(link => {
+      link.addEventListener("click", function (e) {
+        const targetId = this.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        if (targetSection) {
+          e.preventDefault();
+          
+          const headerHeight = header.offsetHeight; // Calcula a altura do header
+  
+          window.scrollTo({
+            top: targetSection.offsetTop - headerHeight, // Corrige a posição da rolagem
+            behavior: "smooth"
+          });
+        }
+      });
+    });
+  });
+  
+
 
